356d6cbf7c6643609e2f57ab6dc63fbb275aaab8
arinmsn <arinmsn@gmail.com>
2019-02-04 18:06:40 -0800

    input_validation works

--- assets/javascript/source.js
+++ assets/javascript/source.js
@@ -1,10 +1,15 @@
 /* --- Improvements ---
 
 1. Dynamically add 'data-name' of 'item-' + dataCount so we can
-recall and -Update- or -Remove- specific entries.\
-set datacount = 0;
+recall and -Update- or -Remove- specific entries.
+set dataCount = 0;
 
-2. Gray out the buttons for -Update- or -Remove-.
+2. Gray out the buttons for -Update- or -Remove-. (Related to #1)
+
+3. Add authentication (Git or Google)
+
+4. Input Validation. Be mindeful of users with disabilities (some cannot see red border!)
+Will also need CSS Styling defined for invalid feedback.
 
 */
 
@@ -31,7 +36,7 @@ $(".submitButton").on("click", function() {
 
     var trainName = $(".trainName").val().trim(); 
     var trainDest = $(".trainDest").val().trim();// Train's destination
-    var firstTrain =$(".firstTrain").val().trim();
+    var firstTrain = $(".firstTrain").val().trim(); 
     var trainFreq = $(".trainFreq").val().trim();// Train Frequency in mins.
 
     console.log("---- Collected user's data ----");
@@ -41,62 +46,78 @@ $(".submitButton").on("click", function() {
     console.log(trainFreq);
     console.log("---- End ----");
 
-    // Local temporary storage
+    //Local temporary storage
     var addedTrain = {
-        name: trainName,
-        trainDest: trainDest,
-        firstTrain: firstTrain,
-        trainFreq: trainFreq
-    }
+         name: trainName,
+         destination: trainDest,
+         time: firstTrain,   /// time = firstTrain      ____________________________________________________________
+         frequency: trainFreq
+        }                                                  
 
-    //Push data to database
-    database.push(addedTrain);
-    console.log(database);
-    // // Input validation 
-    // if (trainName != "" && trainDest != "" &&
-    //     firstTrain == 4 && trainFreq != "") {
+    
+        // Works! - Improvements to follow
+        // Input validation 
+        if (trainName !== "" && trainDest !== "" && firstTrain.length == 4 && trainFreq !== "") {
+            database.push(addedTrain);
+        } else {
+            $("input").val("");
+            return false;  
+        }
 
-    //     // As long as the input boxes are not empty
-    //    // we will push data from user to database.
+        // if (trainName !== "" && trainDest != "" &&
+        // (firstTrain >= 1 && firstTrain <= 2400)  && trainFreq != "") {
+
+    // As long as the input boxes are not empty
+    // we will push data from user to database.
     // database.ref().push({
     //             name: trainName,
     //             destination: trainDest,   //// 2/3 should be trainDest: trainDest,
     //             time: firstTrain,
     //             frequency: trainFreq,
-    //         });
-    // } else {
-    //     // Will figure out how to let user know 
-    //     // Note: Think of accessibility. Red text as well as red think border...
-    //     console.log("User did not enter a valid data!");
-    //     return false;
-    // }
+           //Push data to database
+        // console.log("User did not enter a valid data!");
+        // return false;
+                   
+    // console.log(database);
+    //    } else {
+    // Will figure out how to let user know 
+    // Note: Think of accessibility. Red text as well as red think border...
+    //    database.push(addedTrain); 
+     //   }
     // console.log(database); // Check to see values in database
     // $("input").val("");     // Empty user input area
-    // Clear Textbox area?
-    return false;
+
+    $(".trainName").val().trim(); 
+    $(".trainDest").val().trim();
+    $(".firstTrain").val().trim();
+    $(".trainFreq").val().trim();
+
+    return false; // ------------------------------------ Delete it???
 });
 
-var trainName = "";
-var trainDest = "";
-var firstTrain = "";
-var trainFreq = "";
+var trainName = "";           //  var trainName = ""; 
+var trainDest = "";            // var trainDest = "";
+var firstTrain = "";           // var firstTrain = "";
+var frequency = "";            // var trainFreq = "";
 
 database.on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val());    
 
     var data = childSnapshot.val();
-    var trainName = data.name;
-    var trainDest = data.trainDest;  ///used to be .destination
-    var firstTrain = data.firstTrain;
-    var trainFreq = data.frequency;  ////-------------Fixed
 
-    console.log(name, trainDest, firstTrain, frequency);
+    var name = data.name;
+    var trainDest = data.destination; 
+    var time = data.time;
+    var frequency = data.frequency;  
+
+    console.log(trainName, trainDest, firstTrain, frequency);
 
     // Format the time
 
     var frequency = parseInt(frequency);
     var currentTime = moment();
 
+    // We cam shorten b/c of our earlier ... var data = childSnapshot.val();
     var convertDate = moment(data.time, "HHmm").subtract(1, "years");
 
     var trainTime = moment(convertDate).format("HHmm"); // ********* CONSIDER CHANGING
@@ -117,30 +138,29 @@ database.on("child_added", function(childSnapshot) {
     var nextTrain = moment().add(timeAway, "minutes");
 
     var displayArriving = moment(nextTrain).format("HHmm");
+
+
+
     
     // Appending data ...
     $("#userData").append(
         "<tr><td width='300'>" + data.name +
-        "<td  width='300'>" + data.trainDest +
-        "<td width='100'>" + data.frequency +
-        "<td width='100'>" + data.displayArriving +
-        "<td width='100'>" + data.timeRemain + "</td>" +
+        "<td  width='300'>" + trainDest +
+        "<td width='100'>" + data.frequency +         // <----------Need to update to frequency?
+        "<td width='100'>" + displayArriving +          // <----------Need to update tArrival
+        "<td width='100'>" + timeAway + "</td>" +
         //     <Update> & <Remove> Buttons
         "<td width='200'><div class='stacked-for-medium button-group'>" +
         "<button class='button updateInfo'>Update</button>" +
         "<button class='button removeInfo'>Remove</button></div></td>"
     );
     
-    console.log(nextTrain);
-    console.log(timeRemain);
+    console.log(time);
+    console.log(timeAway);
 
     // BONUS: Update data every 1 minutes
-    // setInterval('window.location.reload()', 60 * 1000);  // 1000 = 1 milliseconds 
+    //setInterval('window.location.reload()', 60 * 1000);  // 1000 = 1 milliseconds 
 });
-
-// Document event handlers for the <Remove> & <Update>
-// <Remove> train data function
-// <Update> train data function
 
 // }); // End of document.ready(f(x))
 
